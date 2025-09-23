5a10528aa65d017b995dd0e367d2a384de74f960
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-09-24 02:04:44 +0500

    day 4 done

--- Day-4/main.tf
+++ Day-4/main.tf
@@ -8,3 +8,16 @@ resource "digitalocean_droplet" "droplet-shaku" {
   size   = "s-1vcpu-1gb"
   name   = "shaku-ka-droplet"
 }
+
+# For locking purposes so that only one person can apply changes at a time
+# resource "aws_dynamodb_table" "terraform-lock" {
+#   name         = "terraform-lock"
+#   billing_mode = "PAY_PER_REQUEST"
+
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+
+#   hash_key = "LockID"
+# }
